9e6973c86369860ba3f048ee876cd9ffb42b4b76
Shubham Verma <shubhh.ab@gmail.com>
2026-04-28 18:37:21 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,14 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: Number(process.env.DB_PORT),
+  
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
